fb89adf3be8bee17191ce65b09f20baea5139419
404xg31 <desimemer11@gmail.com>
2026-06-28 17:51:59 +0530

    Clean up Android.mk by removing empty namespace
    Removed unnecessary empty soong_namespace block and fixed indentation.

--- Android.mk
+++ Android.mk
@@ -1,13 +1,10 @@
-//
-// Copyright (C) 2026 The Android Open Source Project
-// Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-
-soong_namespace {
-}
 #
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),coful)
