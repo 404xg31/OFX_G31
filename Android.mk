9e017ae63dbbb7b2485f1ad5a44207f7e13b323c
404xg31 <desimemer11@gmail.com>
2026-06-28 17:50:05 +0530

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,15 @@
+//
+// Copyright (C) 2026 The Android Open Source Project
+// Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+soong_namespace {
+}
+#
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),coful)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
